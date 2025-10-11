6ae52b46c9f790c445b1799002507091c460bb7b
h3nryyyyyyy <henryyhhoang128@gmail.com>
2025-10-10 21:29:20 -0700

    Update linku-app.js to handle home page

--- linku-app.js
+++ linku-app.js
@@ -2,7 +2,6 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 //run server on local
@@ -13,15 +12,17 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
-
+//sets up ejs engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-//render ejs 
+//render files
 app.get("/", (req, res) => {
-  res.render("linku-index"); // do NOT include .ejs
+  res.render("linku-home");
 });
-
+app.get("/catalog", (req, res) => {
+  res.render("linku-catalog");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
