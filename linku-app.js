a56fce33767feb6b5a9c091dab430541a47520bc
JORDIN <jla622@nau.edu>
2025-10-22 08:49:13 -0700

    Add Socket.io to app for private messages functions

--- linku-app.js
+++ linku-app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import socketIo from "socket.io";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,12 @@ app.use(express.json());
 //run server on local
 const PORT = 3000;
 
+//server for socket connections
+const server = createServer(app);
+//attach socket.io to the server
+const io = socketIo(server);
+
+let connectedUsers= [];
 
 // Setup path for frontend
 const __filename = fileURLToPath(import.meta.url);
@@ -25,4 +32,18 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});
+
+//socket chat functionality
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    //currently shows messages to all
+    console.log('message received:', msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    connectedUsers = connectedUsers.filter(item => item.socketId != socket.id);
+  });
 });
