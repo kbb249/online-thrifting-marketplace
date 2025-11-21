ed1aca781b2b75d3d6176cbff65637ee3ee50412
JORDIN <jla622@nau.edu>
2025-11-21 09:24:25 -0800

    updateprivate_ messages.php

--- cs386/public/private_messages.php
+++ cs386/public/private_messages.php
@@ -1,27 +1,25 @@
 <?php
-
 session_start();
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/php-error.log'); // logs to a file in the same folder
-error_reporting(E_ALL);
 
-<?php
+// Redirect if not logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: linku-login.html");
+    exit();
+}
+
 // Database connection
 $servername = "localhost";
 $username = "linkrlul_OnlineThrifting";
-$password = "MarketplaceDatabase1";
-$dbname = "linkrlul_userActivity";
+$password ="MarketplaceDatabase1";
+$dbname = "linkrlul_userAccount";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get data from POST or session
-$username = $_POST['username'] ?? $_SESSION['username'] ?? 'guest_user';
-$activity_type = $_POST['activity_type'] ?? 'Unknown';
-$details = $_POST['details'] ?? 'No details provided';
-$timestamp = date('Y-m-d H:i:s');
+// Get username from session
+$currentUser = $_SESSION['username'];
 
 ///INSERT UR SHIT HERE
 
